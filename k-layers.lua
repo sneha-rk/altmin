27ae92791ae9918a691239a100c7982abadae22b
Sneha Reddy <cs14btech11020@iith.ac.in>
2017-05-15 10:26:51 +0530

    added options to k-layers

--- k-layers.lua
+++ k-layers.lua
@@ -6,23 +6,32 @@ require 'os'
 require 'gnuplot'
 signal = require('posix.signal')
 
+dataset = 'cifar'
+activation = 'relu'
+
+
 opt = 
 {
-	epochs = 2,
+	n_epochs = 200,
+	epochs = 1,
 	batch_size = 500,
-	print_every = 10,  
-	train_size = 60000,
-	test_size = 1000,
-	learningRate = 1e-2,
-	epsilon = 1e-4,
-	k = 2, --Number of hidden layers
-	sizes = {1024, 100, 100, 1024} --Sizes of inputs in various layers.
-}	
 
+
+	inputSize = 32*32,
+	outputSize = 500,
+	print_every = 10,  
+	learningRate = 1e-3,
+	epsilon = 1e-4,
+	
+	sizes = {}, --Sizes of inputs in various layers.
+	k = 1, --Number of hidden layers
+	train_size = 50000,
+	test_size = 10000,
+	channels = 3
+}	
 if not (assert(#opt.sizes == opt.k + 2, "opt.sizes must have size equal to opt.k + 2")) then
 	os.exit(-1)	
 end
-
 print(opt)
 
 if arg[1] then
@@ -31,6 +40,21 @@ else
 	dir_name = os.date('%B_')..os.date('%D'):sub(4,5)..'_e'..opt.epochs..'_b'..opt.batch_size..'_tr'..opt.train_size..'_tst'..opt.test_size
 	os.execute('mkdir -p '..dir_name)
 end
+
+-- Loading appropriate dataset
+if dataset == 'cifar' then
+	print('Using CIFAR-10 Dataset...')
+	require 'dataset-cifar'
+
+	opt.train_size = 50000
+	opt.channels = 3
+	opt.sizes = {opt.inputSize * opt.channels, opt.outputSize, opt.inputSize * opt.channels}
+else	
+	opt.train_size = 60000
+	opt.channels = 1
+	opt.sizes = {opt.inputSize * opt.channels, opt.outputSize, opt.inputSize * opt.channels}
+end
+
 
 function map(func, array)
 	local new_array = {}
@@ -70,6 +94,15 @@ function load_dataset(train_or_test, count)
 	return data.data, data.labels
 end
 
+-- Loading appropriate dataset
+if dataset == 'cifar' then
+	trainData = cifar.trainData.data:reshape(cifar.trainData.data:size(1), 3*32*32) / 255.0
+	testData, testLabels = cifar.testData.data:reshape(cifar.testData.data:size(1), 3*32*32) / 255.0, cifar.testData.labels
+else	
+	trainData = load_dataset('train', opt.train_size)
+	testData, testLabels = load_dataset('test', opt.test_size)
+end
+
 function test(ds, model, criterion, iter, flag)
 	local t = ds
 	for i = 1, opt.k + 1 do
@@ -88,7 +121,12 @@ function test(ds, model, criterion, iter, flag)
 		for i = 1, 100 do 
 			labels[i] = testLabels[indices[i]]
 			local x = t[indices[i]]
-			x = x:reshape(32,32)
+			if dataset == 'cifar' then
+				x = x:reshape(3, 32,32)
+			else 
+				x = x:reshape(32,32)
+			end 
+
 			local fileName = string.format(dir_name.."/expt_l%d_top%d_epoch%d.jpeg", labels[i] + 1, i, iter)
 			image.save(fileName, x)
 		end
@@ -111,7 +149,6 @@ function trainOneLayer(opt, testDs, ds, ans, model, criterion, k, iter)
 			local cur_ds = ds[{{cur, math.min(cur+opt.batch_size, opt.train_size)},{}}]
 			local cur_ans = ans[{{cur, math.min(cur+opt.batch_size, opt.train_size)},{}}]
 			cur = cur + opt.batch_size
-			j = j + 1
 			
 			--outputs[i+1] is the output of layer i
 			local outputs = {cur_ds}
@@ -121,29 +158,23 @@ function trainOneLayer(opt, testDs, ds, ans, model, criterion, k, iter)
 
 			local output = outputs[opt.k + 2]
 			local loss = criterion:forward(output, cur_ans)
-			
-			-- if j % opt.print_every == 0 then
-			--    print(string.format("Iteration %d, loss %1.6f", j, loss))
-			-- end
+
 			
 			local grad = criterion:backward(output, cur_ans)
-			for i = opt.k + 1, k, -1 do
+			for i = opt.k + 1, 1, -1 do
 				grad = model[i]:backward(outputs[i], grad) 
 			end
 
-			if grad:norm(2) < 1e-7 then
-			   break
-			end
-			
 			model[k].gradInput = grad/torch.sqrt(grad:norm(2))
 			model[k]:updateParameters(opt.learningRate)
 
 			if j % opt.print_every == 0 then
-			   local t, test_loss = test(testDs, model, criterion, (iter * opt.train_size / opt.batch_size) + j, 0)
-			   print(string.format("%d, %1.6f", (iter * opt.train_size / opt.batch_size) + j, torch.mean(test_loss)))
+			   local t, test_loss = test(testDs, model, criterion, ((iter * opt.k + k) * opt.train_size / opt.batch_size) + j, 0)
+			   print(string.format("%d, %1.6f", ((iter * opt.k + k) * opt.train_size / opt.batch_size) + j, torch.mean(test_loss)))
 			end
 
 			train_losses[#train_losses + 1] = loss -- append the new loss
+			j = j + 1
 		end
 	end
 	return model, torch.DoubleTensor(train_losses)
@@ -156,7 +187,7 @@ function alternateMin(opt, model, criterion, trainDs, testDs)
 	end
 	local test_losses = {}
 	local iter = 0
-	while iter < 200 do --Figure out a stopping condition
+	while iter < opt.n_epochs do --Figure out a stopping condition
 		for i = 1, opt.k + 1 do
 			local loss = {}
 			model, loss = trainOneLayer(opt,testDs, trainDs, trainDs:clone(), model, criterion, i, iter)
@@ -171,8 +202,6 @@ function alternateMin(opt, model, criterion, trainDs, testDs)
 		local t, test_loss = test(testDs, model, criterion, iter, 1)
 		test_losses[#test_losses + 1] = test_loss:sum()
 
-		-- print(string.format("Epoch %4d, test loss = %1.6f", iter, torch.mean(test_loss)))
-		-- print(string.format("%d, %1.6f", iter, torch.mean(test_loss)))
 		iter = iter + 1		
 	end
 	return model, train_losses, test_losses
@@ -217,15 +246,19 @@ model = {}
 for i = 1, opt.k + 1 do
 	layer = nn.Sequential()
 	layer:add(nn.Linear(opt.sizes[i], opt.sizes[i+1]))
-	layer:add(nn.ReLU())
+	if activation == 'relu' then
+		layer:add(nn.ReLU())
+	else 
+		layer:add(nn.Sigmoid())
+	end
 	table.insert(model, layer)
 	print(layer)
 end
 
 crit = nn.MSECriterion()
 
-trainData = load_dataset('train', opt.train_size)
-testData, testLabels = load_dataset('test', opt.test_size)
+
+
 m = nil
 tr_loss = nil
 tst_loss = nil
