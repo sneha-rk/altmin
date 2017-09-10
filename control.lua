a623e58a606a2c50f76cca672939fdaf6f3601df
Sneha Reddy <cs14btech11020@iith.ac.in>
2017-09-10 16:18:07 +0530

    Neater control

--- control.lua
+++ control.lua
@@ -7,51 +7,75 @@ require 'os'
 require 'gnuplot'
 signal = require('posix.signal')
 
-dataset = ''
+dataset = 'mnist'
 activation = 'relu'
 
 
 opt = 
 {
-	n_epochs = 1000,
+	n_epochs = 200,
 	epochs = 1,
 	batch_size = 500,
 
 	print_every = 10,  
-	learningRate = 1e-4,
+	learningRate = 5.0e-3,
 	epsilon = 1e-4,
 
 	inputSize = 32*32,
 	outputSize = 500,	
-	sizes = {1024, 750, 500, 750, 1024}, --Sizes of inputs in various layers.
+	sizes = {1024, 750, 500}, --Sizes of inputs in various layers.
 	
-	k = 3, --Number of hidden layers
-	train_size = 50000,
-	channels = 3,
+	k = 2, --Number of hidden layers
+	train_size = 60000,
+	channels = 1,
 	test_size = 10000
 }	
-if not (assert(#opt.sizes == opt.k + 2, "opt.sizes must have size equal to opt.k + 2")) then
-	os.exit(-1)	
-end
-print(opt)
+
+opt.k = #opt.sizes 
 
 if arg[1] then
 	dir_name = arg[1]
 else
-	dir_name = dataset..os.date('%B_')..os.date('%D'):sub(4,5)..os.date('%X'):sub(4,5)..'_e'..opt.epochs..'_b'..opt.batch_size..'_tr'..opt.train_size..'_tst'..opt.test_size
+	dir_name = 'c'..dataset..os.date('%B_')..os.date('%D'):sub(4,5)..os.date('%X'):sub(4,5)..'_e'..opt.epochs..'_b'..opt.batch_size..'_tr'..opt.train_size..'_tst'..opt.test_size
 	os.execute('mkdir -p '..dir_name)
 end
 
 -- Log architecture!
 log_f = io.open("arch_logging.lua", "a+")
-log_f:write(string.format("%s, %1.6f", dir_name, opt.learningRate))
+log_f:write(string.format("%s, %1.6f, %d", dir_name, opt.learningRate, opt.k))
 -- log_f:write(opt)
 log_f:write("\n")
 log_f:close()
 
 
-test_f = io.open(dir_name.. "/test_error.lua", "a+")
-train_f = io.open(dir_name.."/train_error.lua", "a+")
+test_f = io.open(dir_name.. "/test_error.csv", "a+")
+train_f = io.open(dir_name.."/train_error.csv", "a+")
+
+
+
+-- convert rgb to grayscale by averaging channel intensities
+function rgb2gray(im)
+	-- Image.rgb2y uses a different weight mixture
+
+	local dim, w, h = im:size()[1], im:size()[2], im:size()[3]
+	if dim ~= 3 then
+		 print('<error> expected 3 channels')
+		 return im
+	end
+
+	-- a cool application of tensor:select
+	local r = im:select(1, 1)
+	local g = im:select(1, 2)
+	local b = im:select(1, 3)
+
+	local z = torch.Tensor(w, h):zero()
+
+	-- z = z + 0.21r
+	z = z:add(0.21, r)
+	z = z:add(0.72, g)
+	z = z:add(0.07, b)
+	return z
+end
 
 
 -- Loading appropriate dataset
@@ -61,12 +85,21 @@ if dataset == 'cifar' then
 
 	opt.train_size = 50000
 	opt.channels = 3
+elseif dataset == 'gcifar' then
+	print('Using CIFAR-10 Dataset...')
+	require 'dataset-cifar'
+
+	opt.train_size = 50000
+	opt.channels = 1
 else	
 	opt.train_size = 60000
 	opt.channels = 1
 end
 opt.sizes[1] = opt.channels * 1024
-opt.sizes[5] = opt.channels * 1024
+-- opt.sizes[5] = opt.channels * 1024
+
+-- opt.sizes = {1024, 500,1024}
+-- opt.k = 1
 
 
 function map(func, array)
@@ -97,7 +130,11 @@ function load_dataset(train_or_test, count)
 
 	-- vectorize each 2D data point into 1D
 	data.data = data.data:reshape(data.data:size(1), 32*32)
-	data.data = data.data/255.0
+	if dataset == 'gcifar' then 
+		data.data =data.data
+	else
+		data.data = data.data/255.0
+	end
 
 	print('--------------------------------')
 	print(' loaded dataset "' .. train_or_test .. '"')
@@ -111,6 +148,22 @@ end
 if dataset == 'cifar' then
 	trainData = cifar.trainData.data:reshape(cifar.trainData.data:size(1), 3*32*32) / 255.0
 	testData, testLabels = cifar.testData.data:reshape(cifar.testData.data:size(1), 3*32*32) / 255.0, cifar.testData.labels
+elseif dataset == 'gcifar' then 
+	trainData = torch.Tensor(opt.train_size, 32*32) 
+	testData = torch.Tensor(opt.test_size, 32*32)
+	testLabels = cifar.testData.labels
+
+	for i=1,opt.train_size do
+		trainData[i] = image.rgb2y(cifar.trainData.data[i]) 
+	end
+
+	for i=1,opt.test_size do
+		testData[i] = image.rgb2y(cifar.testData.data[i]) 
+	end
+
+	-- image.save('example.jpg', trainData[1])
+	-- trainData = image.rgb2y(cifar.trainData.data) / 255.0
+	-- testData, testLabels = image.rgb2y(cifar.testData.data) / 255.0, cifar.testData.labels
 else	
 	trainData = load_dataset('train', opt.train_size)
 	testData, testLabels = load_dataset('test', opt.test_size)
@@ -132,14 +185,19 @@ function test(ds, encoder, decoder, criterion, iter, flag)
 		for i = 1, 100 do 
 			labels[i] = testLabels[indices[i]]
 			local x = t[indices[i]]
+			local orig = ds[indices[i]]
 			if dataset == 'cifar' then
 				x = x:reshape(3, 32,32)
+				orig = orig:reshape(3, 32,32)
 			else 
 				x = x:reshape(32,32)
+				orig = orig:reshape(32,32)
 			end 
 
 			local fileName = string.format(dir_name.."/expt_l%d_top%d_epoch%d.jpeg", labels[i] + 1, i, iter)
 			image.save(fileName, x)
+			local ofileName = string.format(dir_name.."/expt_l%d_top%d_epoch%dorig.jpeg", labels[i] + 1, i, iter)
+			image.save(ofileName, orig)
 		end
 		local labelFile = string.format(dir_name.."/expt_top10Labels_epoch%d.dat", iter)
 		torch.save(labelFile, torch.ByteTensor(labels))
@@ -198,6 +256,18 @@ function train(opt, encoder, decoder, criterion, trainDs, testDs)
 	local encoder_train_error = {}
 	local decoder_train_error = {}
 	local iter = 0
+
+	signal.signal(signal.SIGINT,function(signum)
+									print("Ctrl-C interrupt in alternateMin! Exiting...")
+									enc = encoder
+									dec = decoder
+									enc_tr_loss = encoder_train_loss
+									dec_tr_loss = decoder_train_loss
+									test_loss = test_losses
+									saveAll()
+									os.exit(128 + signum)
+								end)
+
 	while iter < opt.n_epochs do --Figure out a stopping condition
 		-- print("----- Encoder -----")
 		encoder, decoder, tmp = trainOneLayer(opt, trainDs, encoder, decoder, criterion, testDs, iter)
@@ -210,10 +280,35 @@ function train(opt, encoder, decoder, criterion, trainDs, testDs)
 end
 
 
+
 function saveAll()
 	print('Saving everything...')
-	torch.save(dir_name..'/encoder_weights.dat', enc.modules[1].weight)
-	torch.save(dir_name..'/decoder_weights.dat', dec.modules[1].weight)
+	for i = 1, opt.k - 1 do
+		torch.save(dir_name..string.format('/encoder_weights_%d.dat', i), enc.modules[2*i - 1].weight)
+		torch.save(dir_name..string.format('/decoder_weights_%d.dat', i), dec.modules[2*i - 1].weight)
+	end
+	--Save old stuff as well!
+	if arg[1] then
+		local dn = dir_name..'/old_'..os.date('%B_')..os.date('%D'):sub(4,5)..'_'..os.date('%H%M')
+		local etr = torch.load(dir_name..'/enc_loss.dat')
+		local dtr = torch.load(dir_name..'/dec_loss.dat')
+		local tl  = torch.load(dir_name..'/test_loss.dat')
+		for i=1, #enc_tr_loss do
+			etr[#etr + 1] = enc_tr_loss[i]
+		end
+		enc_tr_loss = etr
+		for i=1, #dec_tr_loss do
+			dtr[#dtr + 1] = dec_tr_loss[i]
+		end
+		dec_tr_loss = dtr
+		for i=1, #tl do
+			tl[#tl + 1] = test_loss[i]
+		end
+		test_loss = tl
+		os.execute('mv '..dir_name..'/enc_loss.dat'..' '..dn)
+		os.execute('mv '..dir_name..'/dec_loss.dat'..' '..dn)
+		os.execute('mv '..dir_name..'/test_loss.dat'..' '..dn)
+	end
 
 	torch.save(dir_name..'/enc_loss.dat', enc_tr_loss)
 	torch.save(dir_name..'/dec_loss.dat', dec_tr_loss)
@@ -292,6 +387,9 @@ end
 
 -- crit = nn.MSECriterion()
 
+-- print(encoder)
+-- print(decoder)
+
 -- encoder
 encoder = nn.Sequential()
 
@@ -308,9 +406,19 @@ print(encoder)
 -- decoder
 decoder = nn.Sequential()
 
-for i = 3, 4 do
-	-- j = opt.k + 1 - i
-	decoder:add(nn.Linear(opt.sizes[i], opt.sizes[i+1]))
+-- for i = 3, 4 do
+-- 	-- j = opt.k + 1 - i
+-- 	decoder:add(nn.Linear(opt.sizes[i], opt.sizes[i+1]))
+-- 	decoder:add(nn.LeakyReLU())
+-- 	if arg[1] then
+-- 		wts = torch.load(dir_name..string.format('/decoder_weights_%d.dat', j))
+-- 		decoder.modules[2*j - 1].weights = wts
+-- 	end
+-- end
+
+for i = opt.k , 2, -1 do
+	j = opt.k + 1 - i
+	decoder:add(nn.Linear(opt.sizes[i], opt.sizes[i-1]))
 	decoder:add(nn.LeakyReLU())
 	if arg[1] then
 		wts = torch.load(dir_name..string.format('/decoder_weights_%d.dat', j))
